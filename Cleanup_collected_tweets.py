72c39582a20dac98412d42108e173f3da69b2439
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-01-31 15:02:37 -0600

    Cleanup collected tweets now removes user names, links and hashtags

--- Cleanup_collected_tweets.py
+++ Cleanup_collected_tweets.py
@@ -9,11 +9,8 @@ import numpy as np
 import string as st 
 
 
-
-
-
 #######################################################################
-# read in text and clean it up
+# read in saved tweets and clean them up
 #######################################################################
 tweets_data = []
 with open('collected_tweets.txt', encoding='utf-8') as f:
@@ -24,5 +21,29 @@ with open('collected_tweets.txt', encoding='utf-8') as f:
 
 print(len(tweets_data))
 
+# remove 'b...'
+cleaned_tweets = []
 for t in tweets_data:
-    print(t[2:-2])
+    cleaned_tweet = (t[2:-2])
+    cleaned_tweets.append(cleaned_tweet)
+
+
+# remove @userName
+# remove links
+# remove hashtags
+from itertools import chain
+import re
+
+cleaned_data = []
+for text in cleaned_tweets:
+    new_text = re.sub(r'#\w+ ?', '', text)      # remove hashtags
+    new_text = re.sub(r'http\S+', '', new_text) # remove links
+    new_text = re.sub(r'@\S+ ?', '', new_text)  # remove user names
+    cleaned_data.append(new_text)
+
+
+
+# write out cleaned up data to disk
+with open('cleaned_tweets.txt', 'a') as myfile:
+        for t in cleaned_data:
+            myfile.write(str(t)+"\n")
