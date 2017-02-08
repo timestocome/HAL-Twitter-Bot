30d4e3036e035be085b73d871c54b5c90e3cecd9
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-02-08 12:51:17 -0600

    Improved cleanup to remove & and unicode chars

--- Cleanup_collected_tweets.py
+++ Cleanup_collected_tweets.py
@@ -1,12 +1,18 @@
+# http://github.com/timestocome
 
+# read in tweets that were written to a disk file and clean them up
+# remove unicode, user names, links, hashtags
 
 
 ############################################################################
 # setup
 #############################################################################
-# import standard libs
 import numpy as np
 import string as st 
+
+from itertools import chain
+import re
+
 
 
 #######################################################################
@@ -19,8 +25,6 @@ with open('collected_tweets.txt', encoding='utf-8') as f:
     
 
 
-print(len(tweets_data))
-
 # remove 'b...'
 cleaned_tweets = []
 for t in tweets_data:
@@ -31,19 +35,20 @@ for t in tweets_data:
 # remove @userName
 # remove links
 # remove hashtags
-from itertools import chain
-import re
-
 cleaned_data = []
 for text in cleaned_tweets:
     new_text = re.sub(r'#\w+ ?', '', text)      # remove hashtags
     new_text = re.sub(r'http\S+', '', new_text) # remove links
     new_text = re.sub(r'@\S+ ?', '', new_text)  # remove user names
+    new_text = re.sub(r'\\x\w\w', '', new_text) # remove unicode chars
+    new_text = re.sub(r'&amp', '', new_text)    # remove & code
     cleaned_data.append(new_text)
 
 
 
+
 # write out cleaned up data to disk
-with open('cleaned_tweets.txt', 'a') as myfile:
+with open('cleaned_tweets.txt', 'w') as myfile:
         for t in cleaned_data:
             myfile.write(str(t)+"\n")
+
